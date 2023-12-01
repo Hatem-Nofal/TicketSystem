84469e59fd14cb9caeeed8c63fcac7fcaf7ca91c
Hatem Nofal <eng.hatemnofal@gmail.com>
2023-12-01 21:52:18 +0200

    Commit

---
+++ TicketSystem.Core/Tickets/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.Domain.Common.Models;
+using TicketSystem.Domain.Tickets.ValueObjects;
+
+namespace TicketSystem.Domain.Tickets;
+public class Ticket : AggregateRoot<TicketId>
+{
+    public Ticket(TicketId id) : base(id)
+    {
+    }
+}
---
+++ TicketSystem.Core/Tickets/ValueObjects/TicketId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.Domain.Common.Models;
+
+namespace TicketSystem.Domain.Tickets.ValueObjects;
+public sealed class TicketId : ValueObject
+{
+    public Guid Value { get;   }
+
+    private TicketId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static TicketId CreateUnique()=> new TicketId(Guid.NewGuid());
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        throw new NotImplementedException();
+    }
+}
